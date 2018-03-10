0deda6825ce539f857405378a9965f56402345fb
Micah Abbott <miabbott@redhat.com>
2018-03-10 13:04:53 -0500

    piratemirror: move from dockerfiles
    Also, update to use $STAGE/$PROD

---
+++ piratemirror/mirror.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -xeou pipefail
+
+prod=${PROD:-/host/repo}
+stage=${STAGE:-/host/staging}
+ref="fedora/27/x86_64/workstation"
+
+if [[ ! -d "$prod" ]] || [[ ! -d "$stage" ]]; then
+    echo "Must have 'staging' and 'repo' directories present"
+    exit 1
+fi
+
+ostree --repo=$stage remote add --if-not-exists --set gpgkeypath=/etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-27-primary onerepo https://kojipkgs.fedoraproject.org/atomic/repo/
+ostree --repo=$stage pull --commit-metadata-only --depth=1 onerepo:$ref
+rhead=$(ostree --repo=$stage rev-parse onerepo:$ref)
+lhead=$(ostree --repo=$prod rev-parse $ref)
+
+if [[ "$rhead" != "$lhead" ]]; then
+    ostree --repo=$stage pull --mirror --depth=1 onerepo:$ref
+    ostree --repo=$stage prune --keep-younger-than="7 days ago" $ref
+    ostree --repo=$stage summary -u
+    /usr/local/bin/rsync-repos --src $stage --dest $prod
+fi
