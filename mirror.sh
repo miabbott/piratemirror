63cefaa8d9d5940659eacfed169319c47f9210dd
Micah Abbott <miabbott@redhat.com>
2018-04-12 02:25:49 +0000

    mirror:  adapt for multiple major releases
    Since the content of each major Fedora release is signed with a
    different GPG key, the 'mirror.sh' script needed to be adapted to use
    each GPG key for each release.  This is despite having a 'unified'
    repo where all the content/refs are available from the same URL.

--- mirror.sh
+++ mirror.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
 set -xeou pipefail
 
+# This script is typically called from the 'docker.io/miabbott/piratemirror'
+# container image, so there are some defaults that are assumed.
+#
 # You need to define a 'prod' and 'stage' directory for the script to run
 # properly.  If you don't pass in those arguments to the script, it assumes
 # you have your directories at '/host/{prod,stage}'.  This is because the
 # script is normally executed in a container with directories bind mounted
-# into the container.
+# into the container at those locations.
+#
+# Additionally, you may set the location of the 'rsync-repos' script, but
+# it defaults to '/root/rsync-repos' where it lives in the container
+
 prod=${1:-/host/prod/}
 stage=${2:-/host/stage/}
-ref="fedora/27/x86_64/workstation"
+rsync_repos=${3:-/root/rsync-repos}
+
+# We are now tracking Fedora 27 and 28 Atomic Workstation
+releases=("27" "28")
 
 if [[ ! -d "$prod" ]] || [[ ! -d "$stage" ]]; then
-    echo "Must have 'staging' and 'repo' directories present"
+    echo "Must have 'prod' and 'stage' directories present"
     exit 1
 fi
 
-# Add the source of truth, mirror the latest commit, prune anything older
-# than 7 days, generate the summary and then rsync to prod.
+# As new major releases are made available, we will need to sync the content
+# for them as well.  Unfortunately, even though we are using a "unified"
+# repo where all the refs are available from one URL, the content of each
+# major release is signed by a different GPG key.  Until we can specify
+# a list of GPG keys to use per remote (see: https://github.com/ostreedev/ostree/issues/773)
+# we have to create a separate remote per release.
 #
-# NOTE: because this is typically run from a container, we assume the
-# location of the 'rsync-repos' script
-ostree --repo=$stage remote add --if-not-exists --set gpgkeypath=/etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-27-primary onerepo https://kojipkgs.fedoraproject.org/atomic/repo/
-ostree --repo=$stage pull --mirror --depth=1 onerepo:$ref
-ostree --repo=$stage prune --keep-younger-than="7 days ago" $ref
+# So for each major release, we add the source of truth, mirror the latest
+# commit, and prune anything older than 7 days.  Then we can generate the
+# summary and rsync to prod.
+
+for rel in "${releases[@]}"; do
+    gpgkeyname=RPM-GPG-KEY-fedora-$rel-primary
+    remote=fedora-$rel-aw
+    ref=fedora/$rel/x86_64/workstation
+    ostree --repo=$stage remote add --if-not-exists --set gpgkeypath=/etc/pki/rpm-gpg/$gpgkeyname $remote https://kojipkgs.fedoraproject.org/atomic/repo/
+    ostree --repo=$stage pull --mirror --depth=1 $remote:$ref
+    ostree --repo=$stage prune --keep-younger-than="7 days ago" $ref
+done
 ostree --repo=$stage summary -u
-/root/rsync-repos --src $stage --dest $prod
+$rsync_repos --src $stage --dest $prod
